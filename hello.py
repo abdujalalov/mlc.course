37ed82bd4ba880ccf056ea5b874ba8a39e2e09a3
someone <someone@someplace.com>
2022-12-31 03:04:50 -0500

    pushing files

---
+++ hello.py
@@ -0,0 +1,6 @@
+columns_to_keep = ['Country', 'Code', 'Year', 'Avg annual hrs', 'GDP per capita (PPP)', 'Num Internet users',
+                   'Mobile cellular subscriptions (per 100 people)', 'Fixed telephone subscriptions (per 100 people)',
+                   'Population', 'Fertility rate', 'Life expectancy', 'Life satisfaction', 'Unemployment rate',
+                   'Mental disorders %', 'Schizophrenia %', 'Bipolar disorder %', 'Depressive disorders % (both)',
+                   'Depressive disorders % (male)', 'Depressive disorders % (female)', 'Eating disorders %',
+                   'Anxiety disorders %', 'Drug use disorders %', 'Self-harm deaths rate', 'Alcohol use disorders %']
